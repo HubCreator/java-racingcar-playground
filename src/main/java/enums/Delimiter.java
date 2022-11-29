7438e8e2d7affae87a164663b88683745f1c7f7a
HubCreator <chocomilk8604@gmail.com>
2022-11-29 13:03:14 +0900

    refactor(Delimiter) : 구조 변경

--- src/main/java/enums/Delimiter.java
+++ src/main/java/enums/Delimiter.java
@@ -1,15 +1,21 @@
 package enums;
 
 public enum Delimiter {
-    COMMA(","), HYPHEN("-");
+    COMMA(",", "쉼표"), HYPHEN("-", "하이픈");
 
     private final String value;
+    private final String information;
 
-    Delimiter(String value) {
+    Delimiter(String value, String information) {
         this.value = value;
+        this.information = information;
     }
 
     public String getValue() {
         return value;
+    }
+
+    public String getInformation() {
+        return information;
     }
 }
--- src/main/java/enums/ViewMessage.java
+++ src/main/java/enums/ViewMessage.java
@@ -1,7 +1,7 @@
 package enums;
 
 public enum ViewMessage {
-    INPUT_RACING_CAR_NAME_MESSAGE(String.format("경주할 자동차 이름을 입력하세요(이름은 쉼표(%s)를 기준으로 구분).", Delimiter.COMMA.getValue())),
+    INPUT_RACING_CAR_NAME_MESSAGE(String.format("경주할 자동차 이름을 입력하세요(이름은 %s(%s)를 기준으로 구분).", Delimiter.COMMA.getInformation(), Delimiter.COMMA.getValue())),
     INPUT_TRY_COUNT_MESSAGE("시도할 회수는 몇회인가요?"),
     RESULT_MESSAGE("실행 결과"),
     RESULT_WHOS_WIN("{0}가 최종 우승했습니다.");
